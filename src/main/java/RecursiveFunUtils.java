2620c4e6452bc1db2e88f5bc8976871acbb556ee
Данил <bekasik@gmail.com>
2013-08-27 14:18:24 +0300

    Recursive functions converted to tail recursion.

--- src/main/java/RecursiveFunUtils.java
+++ src/main/java/RecursiveFunUtils.java
@@ -6,41 +6,32 @@ import java.util.Map;
 public class RecursiveFunUtils {
 
     public static <P, R> List<R> map(List<P> list, Function<P, R> function) {
-        if (list.size() == 0) {
-            return new ArrayList<R>();
-        }
+        return map(list, function, new ArrayList<R>());
+    }
 
-        if (list.size() == 1) {
-            List<R> processed = new ArrayList<R>();
-            processed.add(function.exec(list.get(0)));
-            return processed;
+    public static <P, R> List<R> map(List<P> list, Function<P, R> function, List<R> result) {
+        if (list.size() == 0) {
+            return result;
         }
 
         R obj = function.exec(list.remove(0));
-        List<R> processed = map(list, function);
-        processed.add(0, obj);
-        return processed;
+        result.add(obj);
+        return map(list, function, result);
     }
 
     public static <P> List<P> filter(List<P> list, Function<P, Boolean> comparator) {
-        if (list.size() == 0) {
-            return new ArrayList<P>();
-        }
+        return filter(list, comparator, new ArrayList<P>());
+    }
 
-        if (list.size() == 1) {
-            List<P> processed = new ArrayList<P>();
-            if(comparator.exec(list.get(0))){
-                processed.add(list.get(0));
-            }
-            return processed;
+    public static <P> List<P> filter(List<P> list, Function<P, Boolean> comparator, List<P> result) {
+        if (list.size() == 0) {
+            return result;
         }
 
         P obj = list.remove(0);
-        List<P> processed = filter(list, comparator);
         if (comparator.exec(obj))
-            processed.add(0, obj);
-
-        return processed;
+            result.add(0, obj);
+        return filter(list, comparator, result);
     }
 
     //todo merge sort
@@ -66,28 +57,23 @@ public class RecursiveFunUtils {
     }
 
     public static <P, R> Map<R, List<P>> group(List<P> list, final Function<P, R> function) {
-        if (list.size() == 0) {
-            return new HashMap<R, List<P>>();
-        }
+        return group(list, function, new HashMap<R, List<P>>());
+    }
 
-        if (list.size() == 1) {
-            Map<R, List<P>> result = new HashMap<R, List<P>>();
-            P obj = list.get(0);
-            R field = function.exec(obj);
-            result.put(field, new ArrayList<P>());
-            result.get(field).add(obj);
+    public static <P, R> Map<R, List<P>> group(List<P> list, final Function<P, R> function, Map<R, List<P>> result) {
+        if (list.size() == 0) {
             return result;
         }
 
         P obj = list.remove(0);
         R field = function.exec(obj);
-        Map<R, List<P>> result = group(list, function);
 
         if (result.get(field) == null) {
             result.put(field, new ArrayList<P>());
         }
         result.get(field).add(obj);
-        return result;
+
+        return group(list, function, result);
     }
 
     public static <P> void forEach(List<P> list, Function<P, Void> function) {
